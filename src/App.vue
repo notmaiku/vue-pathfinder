89f60f4c31df517d2d05118e296f211c02b7a3fc
Sh3ddz <Sh3ddz@gmail.com>
2019-09-29 19:46:20 -0400

    Basic grid setup
    classes Grid and Tile created

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Grid />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Grid from "./components/Grid.vue";
+
+// const grid = new Grid(10, 10);
+// grid.toString();
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Grid
   }
 };
 </script>
---
+++ src/components/Grid.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class = "Grid">
+        <h1>{{ "GRID SHOULD BE HERE" }}</h1>
+    <ul id="GRID HERE">
+<!--        <li v-for="(kek, index) in kek[0]" :key="index">-->
+<!--            {{tile.x}}:{{tile.y}},-->
+<!--        </li>-->
+    </ul>
+    </div>
+</template>
+
+<script>
+    class Tile
+    {
+        constructor(x, y)
+        {
+            this.x = x;
+            this.y = y;
+            this.isWall = Boolean;
+        }
+
+        flipIsWall() //Swaps is wall from false -> true or true -> false
+        {
+            this.isWall = !this.isWall;
+        }
+
+        toString()
+        {
+            console.log(this.x+", "+this.y);
+        }
+    }
+
+    class Grid{
+        constructor() {
+            this.width = 10;
+            this.height = 10;
+            this.tiles = [[]];
+            this.initializeTiles();
+        }
+
+        initializeTiles()
+        {
+            for(var x = 0; x < this.width; x++)
+            {
+                this.tiles[x] = [];
+                for(var y = 0; y < this.height; y++)
+                {
+                    this.tiles[x][y] = new Tile(x,y);
+                }
+            }
+        }
+
+        toString()
+        {
+            console.log("width: "+this.width+", height: "+this.height);
+        }
+    }
+
+    export default {
+        name: "Grid",
+        data: function(){
+                return {
+                    Grid: Grid
+                }
+            }
+    }
+</script>
+
+<style scoped>
+
+</style>

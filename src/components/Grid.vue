632fd357ec87e7c8ef5949cba0713676d70c9b5c
notmaiku <notmaiku@protonmail.com>
2019-10-05 21:32:52 -0400

    Tile object updated there is now id prop and isWall is changed on click

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="Grid">
     <table id="GRID">
-      <tr class="row" v-for="(tiles, index) in tiles" :key="index">
-        <ul class="col" v-for="(tiles, index) in tiles" :key="index">
-          <td :id="tiles.id" v-bind:style="tiles"></td>
+      <tr class="col" v-for="(tiles, index) in tiles" :key="index">
+        <ul class="row" v-for="(tiles, index) in tiles" :key="index">
+          <td :id="tiles.id" v-on:click="flipIsWall" ></td>
         </ul>
       </tr>
     </table>
@@ -30,10 +30,6 @@ export default {
     this.initializeTiles();
     this.printTiles();
   },
-  mounted() {
-    this.changeColor();
-  },
-
   methods: {
 
     createArray(length) {
@@ -45,55 +41,33 @@ export default {
         while(i--) arr[length-1 - i] = this.createArray.apply(this, args);
       }
 
-      return arr;
-    },
+      for (var x = 0; x <= this.width; x++) {
+        for (var y = 0; y <= this.height; y++) {
+          this.id = y + "," + x;
+          var Tile = { x: y, y: x, isWall: false, id: this.id };
+          this.tiles[x][y] = Tile;
 
-    initializeTiles() {
-      this.tiles = this.createArray(this.width,this.height);
-
-      console.log(this.tiles.length +" "+this.tiles[0].length);
-
-      for (var y = 0; y <= this.height; y++)
-      {
-        for (var x = 0; x <= this.width; x++)
-        {
-          // console.log(x+", "+y);
-          this.id = x + "," + y;
-          let createdTile = {id: this.id, x: x, y: y, isWall: false};
-          this.tiles[x][y] = createdTile;
+          // document.write("("+x+","+y+")")
         }
       }
     },
-
-    printTiles()
-    {
-      for (var y = 0; y <= this.height; y++)
-      {
-        for (var x = 0; x <= this.width; x++)
-        {
-          var currTile = this.tiles[x][y];
-          console.log(currTile.x+", "+currTile.y);
-        }
-      }
-
-      currTile = this.tiles[3][7];
-      console.log(currTile.x+", "+currTile.y);
-    },
-
-    flipIsWall() {
-      //Swaps is wall from false -> true or true -> false
-      this.isWall = !this.isWall;
-    },
-    toString: () => {
+    toString() {
       console.log(this.x + ", " + this.y);
     },
-    changeColor() {
-      const tiles = document.querySelectorAll("td");
-      tiles.forEach(tile => {
+    flipIsWall() {
+      const tileSelect = document.querySelectorAll("td");
+      tileSelect.forEach(tile => {
         var coord = document.getElementById(tile.id);
         coord.addEventListener("click", event => {
           console.log(coord);
           coord.style.backgroundColor = "#1c1c54";
+          this.tiles.forEach(index => {
+            for(var i = 0; i < 10; i++){
+              if(index[i].id == tile.id){
+                index[i].isWall = true;
+              }
+            }
+          });
         });
       });
     }

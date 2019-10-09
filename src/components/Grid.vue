df856af08ca93f01312436cb6647521b6c988aab
notmaiku <notmaiku@protonmail.com>
2019-10-09 19:50:39 -0400

    you can now drag to make walls

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="Grid">
     <table id="GRID">
-      <tr class="col" v-for="(tiles, index) in tiles" :key="index">
-        <ul class="row" v-for="(tiles, index) in tiles" :key="index">
-          <td v-if="tiles.pathBegin === true" :id="tiles.id" v-bind:style="startColor"></td>
-          <td v-else-if="tiles.pathEnd === true" :id="tiles.id" v-bind:style="stopColor" ></td>
-          <td v-else :id="tiles.id"  v-bind:style="tiles.isWall ? { 'backgroundColor':  'red'} : {'backgroundColor' : 'blue'}" v-on:click="flipIsWall(tiles.x, tiles.y)" ></td>
+      <tr class="col" v-for="(row, rowIndex) in tiles" :key="rowIndex">
+        <ul class="row" v-for="(tile, colIndex) in row" :key="colIndex">
+          <td v-if="tile.pathBegin === true" :id="tile.id" :class="{startColor: tile.pathBegin}"></td>
+          <td v-else-if="tile.pathEnd === true" :id="tile.id" :class="{stopColor: tile.pathEnd}"></td>
+          <td
+            v-else
+            :id="tile.id"
+            :class="{wallOn: tile.isWall}"
+            @mousedown="handleMouseDown(tile.x, tile.y)"
+            @mouseenter="handleMouseEnter(tile.x,tile.y)"
+            @mouseup="handleMouseUp()"
+          ></td>
         </ul>
       </tr>
     </table>
@@ -13,7 +20,6 @@
 </template>
 
 <script>
-import { create } from "domain";
 export default {
   name: "Grid",
   data() {
@@ -21,19 +27,7 @@ export default {
       width: 50,
       height: 20,
       tiles: [],
-      wallOn: {
-        backgroundColor: '#1EB980'
-      },
-      wallOff: {
-        backgroundColor: 'transparent'
-      },
-      startColor: {
-        backgroundColor: 'green'
-      },
-      stopColor:  {
-        backgroundColor: 'red'
-      },
-      
+      mouseIsPressed: false
     };
   },
   created() {
@@ -41,26 +35,40 @@ export default {
   },
   methods: {
     initializeTiles() {
-      for (var x = 0; x < this.height; x++) {
-        this.tiles[x] = [];
-      }
-      for (var x = 0; x < this.width; x++) {
-        for (var y = 0; y < this.height; y++) {
-          this.id = x + "," + y;
-          let createdTile = {
-            id: this.id,
-            x: x,
-            y: y,
-            isWall: true,
-            pathBegin: x === 12 && y === 10,
-            pathEnd: x === 38 && y == 10
-          };
-          this.tiles[y].push(createdTile);
+      for (let y = 0; y < this.height; y++) {
+        const currentRow = [];
+        for (let x = 0; x < this.width; x++) {
+          currentRow.push(this.createTile(x, y));
         }
+        this.tiles.push(currentRow);
       }
     },
-    flipIsWall(x,y) {
-      this.tiles[x][y].isWall = true;
+    createTile(x, y) {
+      return {
+        id: x + "," + y,
+        y,
+        x,
+        isWall: false,
+        pathBegin: x === 12 && y === 10,
+        pathEnd: x === 38 && y == 10
+      };
+    },
+    handleMouseDown(row, col) {
+      this.flipIsWall(col, row)
+      this.mouseIsPressed = true;
+    },
+
+    handleMouseEnter(row, col) {
+      if (!this.mouseIsPressed) return;
+      this.flipIsWall(col, row)
+    },
+
+    handleMouseUp() {
+      this.mouseIsPressed = false;
+    },
+
+    flipIsWall(x, y) {
+      this.tiles[x][y].isWall = !this.tiles[x][y].isWall;
     }
   }
 };

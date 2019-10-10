53fab827f032aa4e73a08a62081510fd0439a6ef
notmaiku <notmaiku@protonmail.com>
2019-10-09 20:10:14 -0400

    commented on code

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -3,6 +3,7 @@
     <table id="GRID">
       <tr class="col" v-for="(row, rowIndex) in tiles" :key="rowIndex">
         <ul class="row" v-for="(tile, colIndex) in row" :key="colIndex">
+          <!-- else if case for start end and regular wall td/objects-->
           <td v-if="tile.pathBegin === true" :id="tile.id" :class="{startColor: tile.pathBegin}"></td>
           <td v-else-if="tile.pathEnd === true" :id="tile.id" :class="{stopColor: tile.pathEnd}"></td>
           <td
@@ -31,18 +32,24 @@ export default {
     };
   },
   created() {
+    // on init create the grid
     this.initializeTiles();
   },
   methods: {
+    // makes the array grid
     initializeTiles() {
+      // rows
       for (let y = 0; y < this.height; y++) {
         const currentRow = [];
+        // col
         for (let x = 0; x < this.width; x++) {
           currentRow.push(this.createTile(x, y));
         }
         this.tiles.push(currentRow);
       }
     },
+    // tile object
+    // these are used for start and end destinations too
     createTile(x, y) {
       return {
         id: x + "," + y,
@@ -53,20 +60,21 @@ export default {
         pathEnd: x === 38 && y == 10
       };
     },
-    handleMouseDown(row, col) {
-      this.flipIsWall(col, row)
+    // called when click just first click
+    handleMouseDown(y, x) {
+      this.flipIsWall(x, y);
       this.mouseIsPressed = true;
     },
-
-    handleMouseEnter(row, col) {
+    // called when pressed and is holding part
+    handleMouseEnter(y, x) {
       if (!this.mouseIsPressed) return;
-      this.flipIsWall(col, row)
+      this.flipIsWall(x, y);
     },
-
+    // called when up to cancel hold
     handleMouseUp() {
       this.mouseIsPressed = false;
     },
-
+    // makes tile object isWall prop to false
     flipIsWall(x, y) {
       this.tiles[x][y].isWall = !this.tiles[x][y].isWall;
     }
